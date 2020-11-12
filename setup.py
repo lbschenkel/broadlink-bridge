0da821b5a83a17c8a5ebd985aa809eaf7af39250
Leonardo Brondani Schenkel <leonardo@schenkel.net>
2020-11-12 16:36:50 +0100

    Version 0.1.6

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='broadlink-bridge',
-    version='0.1.5',
+    version='0.1.6',
     packages=['broadlink_bridge'],
     entry_points={
         'console_scripts': [
